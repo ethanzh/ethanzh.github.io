e8854272693007c0063f46b0703913775f09b81a
ethanzh <ethan.houston@gmail.com>
2018-01-25 10:37:58 -0800

    Added time to read

--- build.py
+++ build.py
@@ -113,7 +113,7 @@ def create_post_html(path):
 
     md_html = md_to_html(text)  # Adds the markdown HTML
 
-    title_html = "<a href=\"/\">" + title + "</a>"
+    title_html = "<a id=\"title_button\" href=\"/\">" + title + "</a>"
 
     reading_time_html = "<p>Reading time: " + reading_time + " minutes</p>"
 
