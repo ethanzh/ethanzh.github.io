cc32fbbfb9e4f8b9a04634779b79e713980176ff
Ethan Houston <ethan.houston@gmail.com>
2018-03-03 20:13:26 -0800

    Labs work

--- js/labs.js
+++ js/labs.js
@@ -1,12 +1,11 @@
-
- // Initialize Firebase
+// Initialize Firebase
 var config = {
-apiKey: "AIzaSyBjy9RhH3obaqdhsq9_GW78swKM3rSLVtI",
-authDomain: "personalsite-backend.firebaseapp.com",
-databaseURL: "https://personalsite-backend.firebaseio.com",
-projectId: "personalsite-backend",
-storageBucket: "",
-messagingSenderId: "379247301983"
+    apiKey: "AIzaSyBjy9RhH3obaqdhsq9_GW78swKM3rSLVtI",
+    authDomain: "personalsite-backend.firebaseapp.com",
+    databaseURL: "https://personalsite-backend.firebaseio.com",
+    projectId: "personalsite-backend",
+    storageBucket: "",
+    messagingSenderId: "379247301983"
 };
 firebase.initializeApp(config);
 
@@ -14,21 +13,47 @@ var database = firebase.database();
 
 function writeUserData(name, email) {
 
-    firebase.database().ref(`/users/test`).set({
-    username: name,
-    email: email
+    firebase.database().ref(`users`).set({
+        username: name,
+        email: email
     });
 }
 
-function saveToFirebase(email) {
-    var emailObject = "ethan@houston.com"
+function retrieveDatabase() {
 
-    firebase.database().ref('subscription-entries').push().set(emailObject)
+    var test = 12345;
+
+
+    var ref = database.ref(`users`).child(`12345`);
+
+    ref.on(`value`, function (snapshot) {
+        var json = snapshot.val();
+
+        console.log(json[`number`])
+    });
+
+}
+
+function saveToFirebase() {
+
+
+    var thing_to_push = {
+
+        username: Math.random().toString().slice(3, 8),
+
+        number: Math.random().toString().slice(3, 8)
+
+    };
+
+    var thing = Math.random().toString().slice(3, 8)
+
+    firebase.database().ref(`users`).child(thing.toString())
+        .set(thing_to_push)
         .then(function (snapshot) {
-            success(); // some success method
+            // pass
         }, function (error) {
             console.log('error' + error);
-            error(); // some error method
+            // pass
         });
 }
 
